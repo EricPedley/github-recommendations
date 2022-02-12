64f85158faf3a56e0c1af2782c47555e0fc65d1b
Eric Pedley <ericpedley@gmail.com>
2022-02-11 21:51:21 -0800

    added stylesheet link and static middleware

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 
 const app = express();
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-    res.render('index', {greeting: 'Hello'});
-  });
-  
-  app.listen(3000, () => console.log('Listening on 3000'));
+  res.render('index', {greeting: 'Hello'});
+});
+
+app.listen(3000, () => console.log('Listening on 3000'));
