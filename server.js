eb3bfd3a0b1a5d9641cf4aa04d20a5937b954fda
Eric Pedley <ericpedley@gmail.com>
2022-02-13 23:20:37 -0800

    added following list

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {greeting: 'Hello'});
+  res.render('index');
 });
 app.get('/user', async (req, res) => {
   const username = req.query.username;
@@ -19,7 +19,13 @@ app.get('/user', async (req, res) => {
       authorization: `username:token ${process.env.GITHUB_TOKEN}`//temporarily using personal access token instead of registering an oauth app.
     }
   }).then(r=>r.json())
-  res.render('user', {username, followers});
+  const following = await fetch(`https://api.github.com/users/${username}/following`,
+  {//https://dev.to/gr2m/github-api-authentication-personal-access-tokens-53kd
+    headers: {
+      authorization: `username:token ${process.env.GITHUB_TOKEN}`//temporarily using personal access token instead of registering an oauth app.
+    }
+  }).then(r=>r.json())
+  res.render('user', {username, followers,following});
 });
 
 app.listen(3000, () => console.log('Listening on 3000'));
