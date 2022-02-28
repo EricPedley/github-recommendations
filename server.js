2b8864a3546cf97de938c63009c6630e9d448dd5
Eric Pedley <ericpedley@gmail.com>
2022-02-27 22:14:44 -0800

    did union of followers and following

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import fetch from 'node-fetch';
+import { getConnections } from './ghapi.js';
+
 if(process.env.NODE_ENVIRONMENT === "development") {
   require('dotenv').config();
 }
@@ -11,21 +12,11 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 app.get('/user', async (req, res) => {
   const username = req.query.username;
-  const followers = await fetch(`https://api.github.com/users/${username}/followers`,
-  {//https://dev.to/gr2m/github-api-authentication-personal-access-tokens-53kd
-    headers: {
-      authorization: `username:token ${process.env.GITHUB_TOKEN}`//temporarily using personal access token instead of registering an oauth app.
-    }
-  }).then(r=>r.json())
-  const following = await fetch(`https://api.github.com/users/${username}/following`,
-  {//https://dev.to/gr2m/github-api-authentication-personal-access-tokens-53kd
-    headers: {
-      authorization: `username:token ${process.env.GITHUB_TOKEN}`//temporarily using personal access token instead of registering an oauth app.
-    }
-  }).then(r=>r.json())
-  res.render('user', {username, followers,following});
+  const connections = await getConnections(username);
+  res.render('user', {username, connections: Object.values(connections)});
 });
 
 app.listen(3000, () => console.log('Listening on 3000'));
